5879ff1d49387e7204a669e7403ea663c7d6d8dd
thescorpiolab <support@thescorpiolab.com>
2019-05-16 22:34:32 +0000

    51619 15

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,5 +1,5 @@
 class GramsController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 
 	def new
 		@gram = Gram.new
@@ -16,14 +16,18 @@ class GramsController < ApplicationController
 	def edit
 		@gram = Gram.find_by_id(params[:id])
 		return render_not_found if @gram.blank?
+		if @gram.user != current_user
+   		render plain: 'Forbidden :(', status: :forbidden
+   	end
 	end
 
 	def update
 	  @gram = Gram.find_by_id(params[:id])
 	  return render_not_found if @gram.blank?
-
+	  if @gram.user != current_user
+	    return render plain: 'Forbidden :(', status: :forbidden
+	  end
 	  @gram.update_attributes(gram_params)
-
 	  if @gram.valid?
 	    redirect_to root_path
 	  else
@@ -32,11 +36,14 @@ class GramsController < ApplicationController
 	end
 
 	def destroy
-    @gram = Gram.find_by_id(params[:id])
-  		return render_not_found if @gram.blank?
-  	@gram.destroy
-  		redirect_to root_path
-  end
+	  @gram = Gram.find_by_id(params[:id])
+	  return render_not_found if @gram.blank?
+	  if @gram.user != current_user
+	    return render plain: 'Forbidden :(', status: :forbidden
+	  end
+	  @gram.destroy
+	  redirect_to root_path
+	end
 
 	def create
 		@gram = current_user.grams.create(gram_params)
@@ -53,8 +60,8 @@ class GramsController < ApplicationController
 		params.require(:gram).permit(:message)
 	end
 
-	def render_not_found
-    render plain: 'Not Found :(', status: :not_found
+	def render_not_found(status=:not_found)
+    render plain: "#{status.to_s.titleize} :(", status: status
   end
 
 end
