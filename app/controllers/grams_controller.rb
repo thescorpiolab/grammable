f239bd20912c1106cf52f77d2da22437da094629
thescorpiolab <support@thescorpiolab.com>
2019-05-23 18:15:26 +0000

    52319 16

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -6,6 +6,7 @@ class GramsController < ApplicationController
 	end
 
 	def index
+		@grams = Gram.all
 	end
 
 	def show
@@ -22,28 +23,26 @@ class GramsController < ApplicationController
 	end
 
 	def update
-	  @gram = Gram.find_by_id(params[:id])
-	  return render_not_found if @gram.blank?
-	  if @gram.user != current_user
-	    return render plain: 'Forbidden :(', status: :forbidden
-	  end
-	  @gram.update_attributes(gram_params)
-	  if @gram.valid?
-	    redirect_to root_path
-	  else
-	    return render :edit, status: :unprocessable_entity
-	  end
-	end
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
 
 	def destroy
-	  @gram = Gram.find_by_id(params[:id])
-	  return render_not_found if @gram.blank?
-	  if @gram.user != current_user
-	    return render plain: 'Forbidden :(', status: :forbidden
-	  end
-	  @gram.destroy
-	  redirect_to root_path
-	end
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
+    @gram.destroy
+    redirect_to root_path
+  end
+
 
 	def create
 		@gram = current_user.grams.create(gram_params)
@@ -57,7 +56,7 @@ class GramsController < ApplicationController
 	private
 	
 	def gram_params
-		params.require(:gram).permit(:message)
+		params.require(:gram).permit(:message, :picture)
 	end
 
 	def render_not_found(status=:not_found)
